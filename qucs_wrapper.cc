7aa2a90411c6adcb0e5a4fb7be2ed262f1f0f81c
Felix Salfelder <felix@salfelder.org>
2015-02-14 18:09:51 +0100

    embryo of a qucs component wrapper.

---
+++ qucs_wrapper.cc
@@ -0,0 +1,140 @@
+/* Copyright (C) 2015 Felix Salfelder
+ *
+ * This file is part of "Gnucap", the Gnu Circuit Analysis Package
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * qucs component wrapper for gnucap
+ * compile with -DQUCS_DEVICE=<name>
+ */
+
+#ifndef QUCS_DEVICE
+# warning no QUCS_DEVICE specified, defaulting to opamp
+# define QUCS_DEVICE opamp
+#endif
+
+#include <globals.h>
+#include <e_storag.h>
+
+// where are these supposed to come from?
+#define NR_TINY 1e-9
+typedef double nr_double_t;
+
+// http://okmij.org/ftp/cpp-digest/computable-include.txt
+// looks ugly, works perfectly
+#define COMPPATH qucs-core/components
+#define _XqYq(x,y) x ## y
+#define _XY(x,y) _XqYq(x,y)
+#define QUOTE(x) #x
+#define MAKESTR(x) QUOTE(x)
+#define MAKEINC(x) COMPPATH/x.h
+#define QDINC MAKEINC(QUCS_DEVICE)
+
+
+#include <qucs-core/complex.h>
+#include <qucs-core/circuit.h>
+#include MAKESTR(QDINC)
+
+#ifndef HAVE_UINT_T
+typedef int uint_t;
+#endif
+
+namespace{
+class DEV_QUCS : public STORAGE {
+	private:
+	  static int _count;
+	public:
+		CARD*	clone()const;
+		DEV_QUCS(): STORAGE(){
+			_comp = new QUCS_DEVICE();
+		}
+		void tr_iwant_matrix(){ incomplete(); }
+		void ac_iwant_matrix(){ incomplete(); }
+		std::string value_name()const {return "nothing";}
+	public: // ports
+		std::string port_name(uint_t)const {incomplete(); return "";}
+		void set_port_by_name(std::string& name, std::string& value);
+		void set_port_by_index(uint_t index, std::string& value);
+		bool port_exists(uint_t i)const {return i < net_nodes();}
+		const std::string port_value(uint_t i)const;
+		void set_port_to_ground(uint_t index);
+
+		std::string current_port_name(uint_t)const {return "";}
+		const std::string current_port_value(uint_t)const;
+		void set_current_port_by_index(uint_t, const std::string&) {unreachable();}
+		bool current_port_exists(uint_t i)const {return i < num_current_ports();}
+
+		uint_t max_nodes()const {unreachable(); return 0;}
+		uint_t min_nodes()const {unreachable(); return 0;}
+		uint_t num_current_ports()const {return 0;}
+		uint_t tail_size()const {return 0;}
+
+		uint_t net_nodes()const {return 0;} //override
+		uint_t ext_nodes()const {return max_nodes();}
+		uint_t int_nodes()const {return 0;}
+		uint_t matrix_nodes()const {return 0;}
+
+		bool has_inode()const {return false;}
+		bool has_iv_probe()const {return false;}
+		bool is_source()const {return false;}
+		bool f_is_value()const {return false;}
+
+		bool node_is_grounded(uint_t i)const {incomplete(); return false;}
+		bool node_is_connected(uint_t i)const {incomplete(); return false;}
+		//--------------------------------------------------------------------
+	public:
+		double tr_review_trunc_error(const FPOLY1* q);
+		double tr_review_check_and_convert(double timestep);
+
+		double tr_involts()const {unreachable(); return 0;}
+		//double  tr_input()const {return tr_involts();}
+		double tr_involts_limited()const {unreachable(); return 0.;}
+		double tr_input_limited()const {return tr_involts_limited();}
+		//double  tr_amps()const;
+		COMPLEX ac_involts()const {unreachable(); return COMPLEX();}
+		//COMPLEX ac_amps()const;
+
+		// int order()const return _trsteporder;}
+		// double error_factor()const {return OPT::trstepcoef[_trsteporder];}
+	private:
+		qucs::circuit* _comp;
+}p0;
+/*--------------------------------------------------------------------------*/
+static DISPATCHER<CARD>::INSTALL
+  d0(&device_dispatcher, QUOTE(QUCS_DEVICE), &p0);
+/*--------------------------------------------------------------------------*/
+CARD*	DEV_QUCS::clone()const
+{ untested();
+	return new DEV_QUCS(*this);
+}
+/*--------------------------------------------------------------------------*/
+const string DEV_QUCS::current_port_value(uint_t)const
+{ incomplete();
+	return "";
+}
+/*--------------------------------------------------------------------------*/
+void DEV_QUCS::set_port_by_name(std::string& /*name*/, std::string& /*value*/)
+{
+	incomplete();
+}
+/*--------------------------------------------------------------------------*/
+void DEV_QUCS::set_port_by_index(uint_t /*index*/, std::string& /*value*/)
+{
+	incomplete();
+}
+/*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+}
