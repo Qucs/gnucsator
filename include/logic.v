3db1df7ad80ff9666946a91194d154945ec7f7f7
Felix Salfelder <felix@salfelder.org>
2018-06-28 16:40:54 +0100

    logic continued
    preliminary hack: use global process, ignore parameters

--- include/logic.v
+++ include/logic.v
@@ -34,14 +34,77 @@ simulator lang=spice
 .options noinsensitive
 
 ******************************************************************************
+* kludge: global logic model. WIP
+.model mos logic ( delay= 1n  rise= 1n  fall= 1n  rs= 100.  rw= 1.G
++ thh= 0.9  thl= 0.1  mr= 5.  mf= 5.  over=10k vmax=1  vmin= 0. )
+******************************************************************************
 .subckt AND(y a b);
 .parameter V=1
 .parameter TR=1
 VV1 dd 0 V
-.model  mos logic ( delay= 1n  rise= 1n  fall= 1n  rs= 100.  rw= 1.G
+.model andmos logic ( delay= 1n  rise= 1n  fall= 1n  rs= 100.  rw= 1.G
 + thh= 0.9  thl= 0.1  mr= 5.  mf= 5.  over=10k vmax=V  vmin= 0. )
-U7  y 0 dd dd a b  mos and
-
+U7 y 0 dd dd a b mos and
 .ends
 ******************************************************************************
+.subckt NAND(y a b);
+.parameter V=1
+.parameter TR=1
+VV1 dd 0 V
+* .model mos logic ( delay= 1n  rise= 1n  fall= 1n  rs= 100.  rw= 1.G
+* + thh= 0.9  thl= 0.1  mr= 5.  mf= 5.  over=10k vmax=V  vmin= 0. )
+U7 y 0 dd dd a b mos nand
+.ends
+******************************************************************************
+.subckt NOR(y a b);
+.parameter V=1
+.parameter TR=1
+VV1 dd 0 V
+* .model mos logic ( delay= 1n  rise= 1n  fall= 1n  rs= 100.  rw= 1.G
+* + thh= 0.9  thl= 0.1  mr= 5.  mf= 5.  over=10k vmax=V  vmin= 0. )
+U7 y 0 dd dd a b mos nor
+.ends
+******************************************************************************
+.subckt XOR(y a b);
+.parameter V=1
+.parameter TR=1
+VV1 dd 0 V
+* .model mos logic ( delay= 1n  rise= 1n  fall= 1n  rs= 100.  rw= 1.G
+* + thh= 0.9  thl= 0.1  mr= 5.  mf= 5.  over=10k vmax=V  vmin= 0. )
+U7 y 0 dd dd a b mos xor
+.ends
+******************************************************************************
+.subckt XNOR(y a b);
+.parameter V=1
+.parameter TR=1
+VV1 dd 0 V
+* .model mos logic ( delay= 1n  rise= 1n  fall= 1n  rs= 100.  rw= 1.G
+* + thh= 0.9  thl= 0.1  mr= 5.  mf= 5.  over=10k vmax=V  vmin= 0. )
+U7 y 0 dd dd a b mos xnor
+.ends
+******************************************************************************
+.subckt OR(y a b);
+.parameter V=1
+.parameter TR=1
+VV1 dd 0 V
+* .model mos logic ( delay= 1n  rise= 1n  fall= 1n  rs= 100.  rw= 1.G
+* + thh= 0.9  thl= 0.1  mr= 5.  mf= 5.  over=10k vmax=V  vmin= 0. )
+U7 y 0 dd dd a b mos or
+.ends
+******************************************************************************
+.subckt Inv(y a);
+.parameter V=1
+.parameter TR=1
+VV1 dd 0 V
+* .model mos logic ( delay= 1n  rise= 1n  fall= 1n  rs= 100.  rw= 1.G
+* + thh= 0.9  thl= 0.1  mr= 5.  mf= 5.  over=10k vmax=V  vmin= 0. )
+U7 y 0 dd dd a mos inv
+.ends
+******************************************************************************
+.hidemodule AND
+.hidemodule NAND
+.hidemodule NOR
+.hidemodule OR
+.hidemodule XOR
+.hidemodule XNOR
 .simulator lang=acs
