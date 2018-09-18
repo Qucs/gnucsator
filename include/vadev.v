a0ca2f305ff27a39cd8b10e29d41db441d0b86e7
Felix Salfelder <felix@salfelder.org>
2018-09-18 16:06:51 +0100

    add missing files and rules

---
+++ include/vadev.v
@@ -0,0 +1,17 @@
+simulator language=verilog
+// This File is part of gnucap-qucs
+// (C) 2018 Felix Salfelder
+// GPLv3
+
+// mapping qucsator names to actual devices
+// "verilog devices" section
+
+//vcresistor:VCR1 _net0 _net1 _net2 _net3 gain="1"
+module vcresistor(1 2 3 4);
+parameter gain=1
+vcr #(.r(gain)) v(3 4 1 2);
+endmodule
+
+`ifdef GNUCAP
+hidemodule vcresistor
+`endif
