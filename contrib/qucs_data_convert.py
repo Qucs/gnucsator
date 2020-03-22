d218ff60345f8af9fdb57be1d0047642253f01c8
Felix Salfelder <felix@salfelder.org>
2020-03-22 08:56:50 +0000

    patch and run qucs_data_convert.py
    GNUCSATOR_POSTPROC_TR=../contrib/qucs_data_convert.py make check -k

--- contrib/qucs_data_convert.py
+++ contrib/qucs_data_convert.py
@@ -3,14 +3,13 @@
 # (c) 2020 Daniel Mulholland
 # License: "In case it's of any use to someone else I include it below" [1]
 
-# faster postprocessing script, replace
-# [ -f $out.tr ] && cat $out.tr | postproc >> $outfile
-# with a reference to the python script
-# [ -f $out.tr ] && cat $out.tr | qucs_data_convert.py $out.tr $outfile
+# faster postprocessing script.
+#$ export GNUCSATOR_POSTPROC_TR=qucs_data_convert.py
 
-# BUG # should be
-#$ export GNUCSATOR_CONVERT_TR=qucs_data_convert.py
-# but it doesn't work.
+# TODO/incomplete?
+# tests fail
+# does it rewrite probe labels?
+# datapoint counter looks wrong.
 
 import sys
 from pathlib import Path
@@ -19,19 +18,19 @@ import pandas as pd
 
 SPACE = '  '
 
-input_file = Path(sys.argv[1])
-output_file = Path(sys.argv[2])
+input_file = sys.stdin # Path(sys.argv[1])
+output_file = sys.stdout # Path(sys.argv[2])
 
 df = pd.read_csv(input_file, sep='\s+', header=0)
 
-print('Reading from file: ' + str(input_file))
+print('Reading from file: ' + str(input_file), file=sys.stderr)
 
 output_file_csv = output_file
-df.to_csv(output_file.parent / Path(output_file.stem + '.csv'))
+# df.to_csv(output_file.parent / Path(output_file.stem + '.csv'))
 
 dep_variables = [v for v in list(df.columns.values) if v != '#Time']
 
-print('Variables: ' + ",".join(dep_variables))
+print('Variables: ' + ",".join(dep_variables), file=sys.stderr)
 
 def write_values(dataset, type='indep', name='time', datasetname='#Time'):
   output = [ ]
@@ -48,9 +47,10 @@ def write_values(dataset, type='indep', name='time', datasetname='#Time'):
   output.append('')
   return "\n".join(output)
 
-print('Writing to: ' + str(output_file))
+print('Writing to: ' + str(output_file), file=sys.stderr)
 
-with open(output_file, 'w') as f:
+#with open(output_file, 'w') as f:
+with output_file as f:
   f.write('<Qucs Dataset >')
   f.write('\n')
   f.write(write_values(df, 'indep', 'time', '#Time'))
