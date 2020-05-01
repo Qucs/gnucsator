4493b9d9ec4ed69bb5da568a8d162434e1d08856
Felix Salfelder <felix@salfelder.org>
2020-05-01 18:45:06 +0100

    make uninstall work with dev-20200626
    this leaves a bug with duplicate transient status.

--- c_uninst.cc
+++ c_uninst.cc
@@ -19,12 +19,21 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
  */
+#define NDEBUG
 #include <globals.h>
 #include <c_comand.h>
 /*--------------------------------------------------------------------------*/
 namespace{
 /*--------------------------------------------------------------------------*/
 class CMD_UNINST : public CMD {
+public:
+	~CMD_UNINST(){
+		for(auto& d : _l){
+			delete d;
+			d = NULL;
+		}
+		_l.resize(0);
+	}
 public:
 	void do_it(CS& cmd, CARD_LIST*) {
 		DISPATCHER<CARD>* d=NULL;
@@ -43,9 +52,16 @@ public:
 		std::string what;
 		while(cmd.more()){
 			cmd >> what;
-			d->uninstall(what);
+			int prev_picky = OPT::picky;
+			OPT::picky = bDANGER;
+			DISPATCHER<CARD>::INSTALL* di =
+				new DISPATCHER<CARD>::INSTALL(d, what, NULL);
+			OPT::picky = prev_picky;
+			_l.push_back(di);
 		}
 	}
+private:
+	std::vector<DISPATCHER<CARD>::INSTALL*> _l;
 } p1;
 DISPATCHER<CMD>::INSTALL d1(&command_dispatcher, "uninst|uninstall", &p1);
 /*--------------------------------------------------------------------------*/
