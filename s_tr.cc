758b3ec3967e2d7ab38063162cc9527b9a1ab54d
Felix Salfelder <felix@salfelder.org>
2020-05-15 23:26:22 +0100

    output stub/wip

---
+++ s_tr.cc
@@ -0,0 +1,64 @@
+/*$Id: s_tr.cc 2016/03/23 al $ -*- C++ -*-
+ * Copyright (C) 2001 Albert Davis
+ * Author: Albert Davis <aldavis@gnu.org>
+ *
+ * This file is part of "Gnucap", the Gnu Circuit Analysis Package
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *------------------------------------------------------------------
+ * tran and fourier commands -- top
+ */
+//testing=script,complete 2007.11.22
+#include "globals.h"
+#include "u_sim_data.h"
+#include "u_status.h"
+#include "s_tr.h"
+#include "s_tr_out.cc"
+#include "s__init.cc"
+#include "s__solve.cc"
+namespace {
+  TRANSIENT p5;
+  DISPATCHER<CMD>::INSTALL      d5(&command_dispatcher, "transient", &p5);
+  DISPATCHER<CKT_BASE>::INSTALL d6(&status_dispatcher,  "transient", &p5);
+}
+/*--------------------------------------------------------------------------*/
+int TRANSIENT::steps_accepted_;
+int TRANSIENT::steps_rejected_;
+int TRANSIENT::steps_total_;
+/*--------------------------------------------------------------------------*/
+void finish_hack(SIM*);
+/*--------------------------------------------------------------------------*/
+void TRANSIENT::do_it(CS& Cmd, CARD_LIST* Scope)
+{
+  _scope = Scope;
+  _sim->set_command_tran();
+  ::status.tran.reset().start();
+  command_base(Cmd);
+  _sim->_has_op = s_TRAN;
+  _scope = NULL;
+  ::status.tran.stop();
+  finish_hack(this);
+}
+/*--------------------------------------------------------------------------*/
+std::string TRANSIENT::status()const
+{
+  return "transient timesteps: accepted=" + to_string(steps_accepted())
+    + ", rejected=" + to_string(steps_rejected())
+    + ", total=" + to_string(steps_total()) + "\n";  
+}
+/*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+// vim:ts=8:sw=2:noet:
---
+++ s_tr_out.cc
@@ -0,0 +1,170 @@
+/*                       -*- C++ -*-
+ * Copyright (C) 2001 Albert Davis
+ *               2020  Szymon Blachuta
+ *               2020  Felix Salfelder
+ *
+ * This file is part of "Gnucap", the Gnu Circuit Analysis Package
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *------------------------------------------------------------------
+ * qucs output functions (print tr only). to be moved to output plugin.
+ */
+#include "u_sim_data.h"
+#include "u_status.h"
+#include "m_wave.h"
+#include "u_prblst.h"
+#include "declare.h"	/* plottr, plopen */
+#include "s__.h"
+/*--------------------------------------------------------------------------*/
+/* SIM::____list: access probe lists
+ */
+const PROBELIST& SIM::alarmlist()const
+{
+  return _probe_lists->alarm[_sim->_mode];
+}
+const PROBELIST& SIM::plotlist()const
+{
+  return _probe_lists->plot[_sim->_mode];
+}
+const PROBELIST& SIM::printlist()const
+{
+  return _probe_lists->print[_sim->_mode];
+}
+const PROBELIST& SIM::storelist()const
+{
+  return _probe_lists->store[_sim->_mode];
+}
+/*--------------------------------------------------------------------------*/
+/* SIM::out: output the data, "keep" for ac reference
+ * x = the x coordinate
+ * print = selected points, "print" to screen, files, etc.
+ * store = all points, for internal postprocessing, measure
+ * keep = after the command is done, dcop for ac
+ */
+void SIM::outdata(double x, int outflags)
+{
+  ::status.output.start();
+  if (outflags & ofKEEP) {
+    _sim->keep_voltages();
+  }else{
+  }
+  if (outflags & ofPRINT) {
+    store_results(x);
+    _sim->reset_iteration_counter(iPRINTSTEP);
+    ::status.hidden_steps = 0;
+  }else{
+    ++::status.hidden_steps;
+  }
+  if (outflags & ofSTORE) {
+    unreachable();
+  }else{
+  }
+  ::status.output.stop();
+}
+/*--------------------------------------------------------------------------*/
+/* SIM::head: print column headings and draw plot borders
+ */
+void SIM::head(double start, double stop, const std::string& col1)
+{
+  if (_sim->_waves) {
+    delete [] _sim->_waves;
+  }else{
+  }
+
+  _sim->_waves = new WAVE [printlist().size()];
+
+
+  if (!plopen(start, stop, plotlist())) {
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    int width = std::min(OPT::numdgt+5, BIGBUFLEN-10);
+    char format[20];
+    //sprintf(format, "%%c%%-%u.%us", width, width);
+    sprintf(format, "%%c%%-%us", width);
+
+    _out.form(format, '#', col1.c_str());
+
+    for (PROBELIST::const_iterator
+	   p=printlist().begin();  p!=printlist().end();  ++p) {
+      _out.form(format, ' ', p->label().c_str());
+    }
+    _out << '\n';
+  }else{
+  }
+}
+/*--------------------------------------------------------------------------*/
+/* SIM::print_results: print the list of results (text form) to _out
+ * The argument is the first column (independent variable, aka "x")
+ */
+void SIM::print_results(double x)
+{
+  unreachable();
+}
+/*--------------------------------------------------------------------------*/
+/* SIM::alarm: print a message when a probe is out of range
+ */
+void SIM::alarm(void)
+{
+  _out.setfloatwidth(OPT::numdgt, OPT::numdgt+6);
+  for (PROBELIST::const_iterator
+	 p=alarmlist().begin();  p!=alarmlist().end();  ++p) {
+    if (!p->in_range()) {
+      _out << p->label() << '=' << p->value() << '\n';
+    }else{
+    }
+  }
+}
+/*--------------------------------------------------------------------------*/
+/* SIM::store: store data in preparation for post processing
+ */
+void SIM::store_results(double x)
+{
+  int ii = 0;
+  for (PROBELIST::const_iterator
+	 p=storelist().begin();  p!=storelist().end();  ++p) {
+    _sim->_waves[ii++].push(x, p->value());
+  }
+}
+/*--------------------------------------------------------------------------*/
+void finish_hack(SIM* s)
+{
+  // fixme: this is currently within the obsolete control script
+  //outFile << "<Qucs Dataset>"  << endl; //"<Qucs Dataset 0.0.19>"
+  //
+  auto& PL = SIM::_probe_lists->store[s_TRAN];
+  unsigned number_of_probes = PL.size();
+
+  auto& outFile = std::cout;
+  unsigned n=17; //todo
+  outFile << "<indep time " << n << ">" << std::endl; //<indep time 19>
+  std::vector<WAVE::const_iterator> w;
+
+  for(unsigned i=0; i<number_of_probes; ++i){
+    w.push_back(CKT_BASE::_sim->_waves[i].begin());
+  }
+
+  ... sort this out
+  for(unsigned i=0; i<number_of_probes; ++i){
+    // "v(_net0)" -> "_net0"
+    std::string itm = PL[i];
+    outFile << "<dep " << itm.substr(2, itm.length()-3 ) << "."<< (char)toupper(itm.at(0)) << "t time>" <<endl;
+    wrData(data[px], outFile);
+    outFile << "</dep>" << endl;
+  }
+  // wrData(data[0], outFile);
+  outFile << "</indep>" << endl;
+}
+/*--------------------------------------------------------------------------*/
+// vim:ts=8:sw=2:noet:
