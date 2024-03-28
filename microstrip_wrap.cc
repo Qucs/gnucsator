ebd86db3d40a6006a247f2aec5047190e939bc30
Felix Salfelder <felix@salfelder.org>
2024-03-28 00:00:00 +0000

    dispatch msline & MLIN SP test

--- microstrip_wrap.cc
+++ microstrip_wrap.cc
@@ -9,6 +9,7 @@
 #include "microstrip/mslange.h"
 #include "microstrip/msopen.h"
 #include "microstrip/msstep.h"
+// #include "microstrip/mstee.h"
 
 namespace {
 
@@ -31,15 +32,18 @@ DISPATCHER<CARD>::INSTALL d8(&device_dispatcher, mscoupled::cirdef.type, &c8);
 
 mslange c11;
 DISPATCHER<CARD>::INSTALL d11(&device_dispatcher, mslange::cirdef.type, &c11);
+msline c12;
+DISPATCHER<CARD>::INSTALL d12(&device_dispatcher, msline::cirdef.type, &c12);
 
-// CIR_MSLINE = 0;
 // CIR_MSMBEND = 0;
 msopen c14;
 DISPATCHER<CARD>::INSTALL d14(&device_dispatcher, msopen::cirdef.type, &c14);
 // CIR_MSRSTUB = 0;	
 msstep c16;
 DISPATCHER<CARD>::INSTALL d16(&device_dispatcher, msstep::cirdef.type, &c16);
-// CIR_MSTEE = 0;
+//mstee c17;
+//DISPATCHER<CARD>::INSTALL d17(&device_dispatcher, mstee::cirdef.type, &c17);
+//
 // CIR_MSVIA = 0;	
 // CIR_SPIRALIND = 0;	
 
