07a1440064dc62d400fe017c0459c71ac736ea37
Felix Salfelder <felix@salfelder.org>
2025-05-12 00:00:00 +0000

    add GND, net. bump STATUS

---
+++ include/suppl.v
@@ -0,0 +1,13 @@
+simulator language=verilog
+// This File is part of gnucap-qucs
+// (C) 2025 Felix Salfelder
+// GPLv3+
+//
+// schematic devices, not used in flat "Qucsator" netlists
+
+module net(.a(i), .b(i));
+endmodule
+
+module GND(.p(g));
+   ground g;
+endmodule
